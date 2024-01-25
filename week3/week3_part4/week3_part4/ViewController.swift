3a1738165ced6e8328e88bf3ba3a53cbf4edeb70
Kyle Lu <61376338+kkylelu@users.noreply.github.com>
2024-01-25 11:44:12 +0800

    update log in&Sign up UI

--- week3/week3_part4/week3_part4/ViewController.swift
+++ week3/week3_part4/week3_part4/ViewController.swift
@@ -8,7 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBAction func changeStatus(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            checkLabel.textColor = UIColor.gray
+            checkTextField.backgroundColor = UIColor.darkGray
+            checkTextField.isEnabled = false
+        } else {
+            checkLabel.textColor = UIColor.black
+            checkTextField.isEnabled = true
+            checkTextField.backgroundColor = UIColor.white
+        }
+        
+    }
+    
+    @IBOutlet weak var accountTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var checkTextField: UITextField!
+    
+    @IBOutlet weak var checkLabel: UILabel!
+    
+    @IBAction func myButton(_ sender: Any) {
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
